3f4b9ef6a96bf18544fbeb24c61473283286883d
Minecraaaft <maxmagi@hotmail.com>
2019-10-26 21:00:09 +0200

    message class started

--- src/message/Message.java
+++ src/message/Message.java
@@ -1,4 +1,21 @@
 package message;
 
 public class Message {
+    private String language;
+
+    public Message(String language) {
+        this.language = language;
+    }
+
+    public String getMessage(String playerName) {
+        String message = "";
+        switch (language) {
+            case "English":
+                message = "It's " + playerName + "s turn.";
+            case "Dansk":
+                message = "Det er " + playerName + "s tur.";
+        }
+
+        return message;
+    }
 }
