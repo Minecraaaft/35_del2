f803743b11e1c23e34865d239c2a9562eb4a4bb6
Mohammad <s184174@student.dtu.dk>
2019-10-26 10:50:39 +0200

    Die class: dice created

--- src/DieCup/Die.java
+++ src/DieCup/Die.java
@@ -1,5 +1,24 @@
 package DieCup;
 
+import java.util.Random;
+
 public class Die {
+    // Attributter
+    private int faceValue;
+
+    // Konstruktør
+    public Die(){
+    }
+
+    public int roll(){
+        Random random = new Random();
+        faceValue =random.nextInt(6)+1;
+        return faceValue;
+    }
+
+    public int getFaceValue(){
+        return faceValue;
+    }
+
 
 }
