24b2e4727aa954152b7e9e2a8741e959d2cb2c54
MerithzDTU <s195480@student.dtu.dk>
2019-10-28 11:03:31 +0100

    Spiller klassen

--- src/player/Player.java
+++ src/player/Player.java
@@ -4,5 +4,23 @@ public class Player {
 
     // Attributes
     private String name;
+    private static int playerNumber;
+    private Balance balance = new Balance();
+    private int fieldPos;
+    private boolean hasLost;
 
+    public Player(){
+        this.name = "Player" + ++playerNumber;
+        this.fieldPos = 0;
+
+        this.hasLost = false;
+    }
+
+    public boolean isHasLost(){
+        return hasLost;
+    }
+    //Setters
+    public void setHasLost(boolean hasLost){
+       this.hasLost = hasLost; 
+    }
 }
